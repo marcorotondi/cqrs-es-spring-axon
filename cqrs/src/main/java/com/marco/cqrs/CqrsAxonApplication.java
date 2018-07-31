11fe40b9d6facfad208011732500e6df5509bf35
rot <rotondi@intre.it>
2018-07-31 16:56:16 +0200

    Update git ignore

---
+++ cqrs/src/main/java/com/marco/cqrs/CqrsAxonApplication.java
@@ -0,0 +1,12 @@
+package com.marco.cqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsAxonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsAxonApplication.class, args);
+	}
+}
